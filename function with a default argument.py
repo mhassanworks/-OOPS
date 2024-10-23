de804b957caa5d26c24210cda614e84503543e06
MOHAMMAD HASSAN <159164628+mhassanworks@users.noreply.github.com>
2024-10-23 21:26:36 +0530

    Create function with a default argument.py

---
+++ function with a default argument.py
@@ -0,0 +1,9 @@
+ # Function to greet a person with a default name
+def greet(name="Guest"):
+    print(f"Hello, {name}!")
+
+# Calling the function without passing an argument
+greet()
+
+# Calling the function with an argument
+greet("Hassan")
