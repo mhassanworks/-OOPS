67fe8de9135956db3a5b2ab6d37984331b8b16b2
MOHAMMAD HASSAN <159164628+mhassanworks@users.noreply.github.com>
2025-02-09 21:49:19 +0530

    Create threading.java

---
+++ threading.java
@@ -0,0 +1,22 @@
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - Count: " + i);
+            try {
+                Thread.sleep(1000); // Pause for a second
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        
+        t1.start(); // Start thread 1
+        t2.start(); // Start thread 2
+    }
+}
